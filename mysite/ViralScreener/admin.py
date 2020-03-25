d5c382e16267482595d79024c5555984f73e1c3e
Micah Guttman <memtech3@gmail.com>
2020-03-25 17:16:06 -0500

    added models for employee visitor and homemessage

--- mysite/ViralScreener/admin.py
+++ mysite/ViralScreener/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import AlertMessage, Employee
+from .models import AlertMessage, Employee, Visitor, HomeMessages
 # Register your models here.
 admin.site.register(AlertMessage)
 admin.site.register(Employee)
+admin.site.register(Visitor)
+admin.site.register(HomeMessages)
--- mysite/ViralScreener/models.py
+++ mysite/ViralScreener/models.py
@@ -10,9 +10,28 @@ class AlertMessage(models.Model):
     def __str__(self):
         return self.name
 
+class HomeMessages(models.Model):
+    name = models.CharField(max_length=200)
+    header = models.CharField(max_length=200)
+    content = models.TextField()
+
 class Employee(models.Model):
+    id = models.AutoField(primary_key=True),
     FirstName = models.CharField(max_length=200)
     MiddleName = models.CharField(max_length=200, blank=True)
     LastName = models.CharField(max_length=200)
     EmployeeID = models.CharField(max_length=200, blank=True)
     models.UniqueConstraint(fields=['FirstName', 'MiddleName', 'LastName'], name='unique_person')
+    readonly_fields = ('id',)
+    def __str__(self):
+        return ('E'+str(self.id))
+
+class Visitor(models.Model):
+    id = models.AutoField(primary_key=True),
+    FirstName = models.CharField(max_length=200)
+    MiddleName = models.CharField(max_length=200, blank=True)
+    LastName = models.CharField(max_length=200)
+    models.UniqueConstraint(fields=['FirstName', 'MiddleName', 'LastName'], name='unique_person')
+    readonly_fields = ('id',)
+    def __str__(self):
+        return ('V'+str(self.id))
